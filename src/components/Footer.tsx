0ac8cd775050225d9527eb752220e2fe419df706
saptarshi dutta <saptarshidutta599@gmail.com>
2023-09-16 12:34:45 +0530

    navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+function Footer() {
+  return (
+  <>
+    <div className="container"></div>
+    <footer  className="min-height: 5vh">
+        
+        <section className="ft-main">
+          <div className="ft-main-item">
+            <h2 className="ft-title">About</h2>
+            <ul>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Portfolio</a></li>
+              <li><a href="#">Pricing</a></li>
+              <li><a href="#">Customers</a></li>
+              <li><a href="#">Careers</a></li>
+            </ul>
+          </div>
+          <div className="ft-main-item">
+            <h2 className="ft-title">Resources</h2>
+            <ul>
+              <li><a href="#">Docs</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">eBooks</a></li>
+              <li><a href="#">Webinars</a></li>
+            </ul>
+          </div>
+          <div className="ft-main-item">
+            <h2 className="ft-title">Contact</h2>
+            <ul>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Sales</a></li>
+              <li><a href="#">Advertise</a></li>
+            </ul>
+          </div>
+          <div className="ft-main-item">
+            <h2 className="ft-title">Stay Updated</h2>
+            <p id="change">Subscribe to our newsletter to get our latest news.</p>
+            <form>
+              <input type="email" name="email" placeholder="Enter email address" />
+             
+              <button type="button" id="demo">Subscribe</button>
+            </form>
+          </div>
+        </section>
+      
+        <section className="ft-social">
+          <ul className="ft-social-list">
+            <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i className="fab fa-github"></i></a></li>
+            <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+            <li><a href="#"><i className="fab fa-youtube"></i></a></li>
+          </ul>
+        </section>
+      
+        <section className="ft-legal">
+          <ul className="ft-legal-list">
+            <li><a href="#">Terms &amp; Conditions</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li>&copy; 2019 Copyright Nowrap Inc.</li>
+          </ul>
+        </section>
+      </footer>
+      </>
+
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+export function NavbarDefault() {
+ 
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Link href={''}
+        as="li"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >Pages</Link>
+
+      <Link href={''}
+        as="li"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >Account</Link>
+
+      <Link href={''}
+        as="li"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >Docs</Link>
+      <Link href={''}
+        as="li"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >Blocks</Link>
+    </ul>
+  );
+ 
+  return (
+    <nav className="py-2 px-4 lg:px-8 lg:py-4 shadow-md">
+      <div className="w-screen mx-auto flex items-center justify-center">
+        <div className="hidden lg:block ">{navList}</div>
+      </div>
+    </nav>
+  );
+}
