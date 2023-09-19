188ff9b73c3bbafe8c66e3a049353a683792b8fa
saptarshi dutta <saptarshidutta599@gmail.com>
2023-09-19 11:46:57 +0530

    landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily:{
+        nanum:['Nanum Gothic Coding'],
+        cabin:['Cabin Sketch']
+      }
     },
   },
   plugins: [],
